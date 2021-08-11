a8560f9815c23a084700d52a9d1f7f08d474a548
Ryan <rzimmerman@talusanalytics.com>
2021-08-11 12:50:52 -0600

    Add common packages, add config options in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,34 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "talus-gatsby-starter",
+    siteUrl: 'https://example.talusanalytics.com/',
+    title: 'Talus Analytics',
+    cookieConsent: {
+      cookieMessage:
+        'Talus sites use cookies to ensure you get the best experience possible.',
+      buttonColor: '',
+      backgroundColor: '',
+    },
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-mdx',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        trackingId: `G-XXXXXXXXXX`,
+        anonymize: true,
+        head: false,
       },
-      __key: "pages",
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
     },
   ],
-};
+}
