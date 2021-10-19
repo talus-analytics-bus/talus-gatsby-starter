c0798e6d8d7ffc2e66cff12a93d605626232fbef
Ryan <rzimmerman@talusanalytics.com>
2021-10-19 13:02:08 -0600

    Adding new index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,24 @@
 import React from 'react'
 
-import { useStaticQuery, graphql } from 'gatsby'
-
 import FigmaProvider from '../figma/FigmaProvider'
 import Main from '../components/layout/Main'
 
-interface IndexPageData {
-  allAirtable: {
-    edges: {
-      node: {
-        data: {
-          Name: string
-          Notes: string
-          Status: string
-          url: string
-        }
-      }
-    }[]
-  }
-}
+import useIndexPageData from '../cmsHooks/useIndexPageData'
+import AirtableCMSText, { getCMSText } from '../airtable-cms/AirtableCMSText'
 
 const IndexPage = (): JSX.Element => {
-  const data: IndexPageData = useStaticQuery(graphql`
-    query indexQuery {
-      allAirtable {
-        edges {
-          node {
-            data {
-              Name
-              Notes
-              Status
-              url
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  const airtableData = data.allAirtable.edges[0].node.data
+  const data = useIndexPageData()
 
   return (
     // all pages should be wrapped in the FigmaProvider
     <FigmaProvider>
       <Main>
-        <h1>Talus Analytics Gatsby Starter</h1>
+        <h1>
+          <AirtableCMSText name="H1" data={data} />
+        </h1>
         <p>
-          {airtableData.Notes.trim()}, from{' '}
-          <a href={airtableData.url}>this table.</a>
+          <AirtableCMSText name="Example Text" data={data} />
+          <a href={getCMSText(data, 'Airtable URL')}>this table.</a>
         </p>
       </Main>
     </FigmaProvider>
