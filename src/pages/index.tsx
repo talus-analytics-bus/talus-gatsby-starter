68b106b2f4bb81d794a3ecf2f6bf39a4b5cbd064
Ryan <rzimmerman@talusanalytics.com>
2021-10-19 12:49:11 -0600

    Fixing index.tsx (sort of)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 import { useStaticQuery, graphql } from 'gatsby'
 
-import Layout from '../components/Layout/Layout'
+import FigmaProvider from '../figma/FigmaProvider'
+import Main from '../components/layout/Main'
 
 interface IndexPageData {
   allAirtable: {
@@ -19,7 +20,7 @@ interface IndexPageData {
   }
 }
 
-const IndexPage: React.FC = () => {
+const IndexPage = (): JSX.Element => {
   const data: IndexPageData = useStaticQuery(graphql`
     query indexQuery {
       allAirtable {
@@ -40,13 +41,16 @@ const IndexPage: React.FC = () => {
   const airtableData = data.allAirtable.edges[0].node.data
 
   return (
-    <Layout>
-      <h1>Talus Analytics Gatsby Starter</h1>
-      <p>
-        {airtableData.Notes.trim()}, from{' '}
-        <a href={airtableData.url}>this table.</a>
-      </p>
-    </Layout>
+    // all pages should be wrapped in the FigmaProvider
+    <FigmaProvider>
+      <Main>
+        <h1>Talus Analytics Gatsby Starter</h1>
+        <p>
+          {airtableData.Notes.trim()}, from{' '}
+          <a href={airtableData.url}>this table.</a>
+        </p>
+      </Main>
+    </FigmaProvider>
   )
 }
 
