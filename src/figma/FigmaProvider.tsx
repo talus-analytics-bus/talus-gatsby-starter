3f5c8cda63bb9c6bd2a67107b5a71ef015ea5cb0
Ryan <rzimmerman@talusanalytics.com>
2021-10-19 12:25:27 -0600

    Proper typing of FigmaProvider; removing React.FC

--- src/figma/FigmaProvider.tsx
+++ src/figma/FigmaProvider.tsx
@@ -3,14 +3,16 @@ import { ThemeProvider } from 'styled-components'
 
 import { semanticStyles } from './semanticStyles.module.scss'
 
-import './fonts.css'
-
 // inline webpack require because it's easier that way in gatsby
 const figmaStyles = require('sass-extract-loader?{"plugins":["sass-extract-js"]}!./styles.scss')
 
-const FigmaProvider: React.FC = props => (
+const FigmaProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}): JSX.Element => (
   <ThemeProvider theme={figmaStyles}>
-    <div className={semanticStyles}>{props.children}</div>
+    <div className={semanticStyles}>{children}</div>
   </ThemeProvider>
 )
 
