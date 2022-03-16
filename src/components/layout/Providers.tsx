9daaba405d89007980f3e22e9c6ae4bd9a53ff4b
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-03-16 02:28:31 +0000

    Revert "Adding new fonts.css file in static assets dir"
    This reverts commit 7f647a5051320c91f74baf85f4c488baa0b1fae4.

--- src/components/layout/Providers.tsx
+++ src/components/layout/Providers.tsx
@@ -1,30 +1,11 @@
 import React from 'react'
 
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
 import CMS from '@talus-analytics/library.airtable-cms'
+import FigmaProvider from '../../figma/FigmaProvider'
 
-// import textStyles from '../figma/textStyles'
-// import textStyles from '../../figma/textStyles'
-import textStyles from 'figma/textStyles'
-import colorPalette from 'figma/colorPalette'
-
-import useIconsQuery from 'cmsHooks/useIconsQuery'
-import useSiteMetadataQuery from 'cmsHooks/useSiteMetadataQuery'
-import getTrackingId from 'utilities/trackingId'
-
-import '../../../static/assets/fonts/fonts.css'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: "Open Sans", Arial, Helvetica, sans-serif;
-  }
-  * {
-    box-sizing: border-box;
-  }
-`
+import useIconsQuery from '../../cmsHooks/useIconsQuery'
+import useSiteMetadataQuery from '../../cmsHooks/useSiteMetadataQuery'
+import getTrackingId from '../../utilities/trackingId'
 
 // site-wide contexts for themes, icons, and metadata
 const Providers = ({ children }: { children: React.ReactNode }) => {
@@ -36,10 +17,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <CMS.IconProvider data={icons}>
       <CMS.SiteMetadataProvider data={siteMetadata} trackingId={trackingId}>
-        <ThemeProvider theme={{ ...textStyles, ...colorPalette }}>
-          <GlobalStyle />
-          {children}
-        </ThemeProvider>
+        <FigmaProvider>{children}</FigmaProvider>
       </CMS.SiteMetadataProvider>
     </CMS.IconProvider>
   )
